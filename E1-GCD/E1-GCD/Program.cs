b00017e2f1949f28d77dea607a4ad1f4f6b80db1
raddevus <1radeutsch1@gmail.com>
2026-06-08 15:29:03 -0400

    Added basic code to get two integers from user.

--- E1-GCD/E1-GCD/Program.cs
+++ E1-GCD/E1-GCD/Program.cs
@@ -1,1 +1,17 @@
-﻿Console.WriteLine("Hello, World!");
+﻿
+try {
+   Console.WriteLine("Please enter the first of two integers > 0.");
+   int m = Convert.ToInt32(Console.ReadLine());
+   Console.WriteLine("Please enter the second of two integers > 0.");
+   int n = Convert.ToInt32(Console.ReadLine());
+   // insure that n is smallest of two integers
+   if (m < n){ 
+      var temp = m;
+      m = n;
+      n = temp;
+   }
+   Console.WriteLine($"Starting with m:{m} n:{n}");
+}
+catch (Exception ex){
+   Console.WriteLine($"Error: {ex.Message}");
+}
